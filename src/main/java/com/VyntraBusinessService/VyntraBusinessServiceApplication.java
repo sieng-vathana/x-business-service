a911d01ffa565be9bb52b54ed504e45ef6284491
Vathana Programmer <vathana@example.com>
2026-05-29 19:17:27 +0700

    Initial commit

---
+++ src/main/java/com/VyntraBusinessService/VyntraBusinessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.VyntraBusinessService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyntraBusinessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyntraBusinessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VyntraBusinessService/VyntraBusinessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VyntraBusinessService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyntraBusinessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
